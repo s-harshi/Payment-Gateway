1274aa4892ffec9092e216d14eb7c0e44d3009a6
hola-ryuzaki <hello.ryuzaki@gmail.com>
2023-06-17 11:35:51 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+dots = document.querySelector('.dots')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-list')
+rightNav = document.querySelector('.rightNav')
+
+
+dots.addEventListener('click',()=>{
+
+    rightNav.classList.toggle('v-class-resp');
+    navList.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-nav-resp');
+})
